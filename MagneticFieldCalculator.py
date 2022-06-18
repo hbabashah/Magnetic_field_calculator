06a68c8e2371c41412edec2278b15ed90471a0cc
hbabashah <75782291+hbabashah@users.noreply.github.com>
2022-06-18 16:27:21 +0200

    Update MagneticFieldCalculator.py

--- MagneticFieldCalculator.py
+++ MagneticFieldCalculator.py
@@ -6,21 +6,71 @@ Created on Thu Jun 16 16:54:48 2022
 """
 
 import numpy as np
+mu0=4*np.pi*1e-7
 def B_wire(I=1,r=np.array([1])):
+    '''
+    Parameters
+    ----------
+    I : integer, current (amper)
+    r : integer, radius (meter)
+
+    Returns
+    -------
+        magnetic field. (T)
+
+    '''
     r=np.array(r)
-    mu0=4*np.pi*1e-7
-    # I is amp
-    # r is radius m
     return mu0*I/(r*2*np.pi)
 
 def B_solenoid_center(I=1,L=1,N=100):
-    mu0=4*np.pi*1e-7
+    '''
+    Parameters
+    ----------
+    I : float, current (A)
+    L : float, length (m)
+    N : Number of turns, optional
+    Returns
+    -------
+     magnetic field
+    '''
+    
+
     # I is amp
     # r is radius m
     k=1 #relative permeability of the core
     return k*mu0*I*N/L
 
+def B_flat_coil(I=1,R=1,N=100):
+    '''
+    Parameters
+    ----------
+    I : float, current (A)
+    L : float, length (m)
+    N : Number of turns, optional
+    Returns
+    -------
+     magnetic field
+    '''
+    
+    # I is amp
+    # r is radius m
+    k=1 #relative permeability of the core
+    return k*mu0*I*N/(2*R)
+def L_coil(N=10,R=1,L=1):
+    '''
+    Parameters
+    ----------
+    N : number of turns
+    A : Coil area (m^2)
+    L : Coil Length (m)
 
+    Returns
+    -------
+    None.
+    
+    '''
+    A=np.pi*R**2
+    return mu0*N**2*A/L        
 if __name__ == "__main__":
     import pylab as pl
     r=np.linspace(.01,0.09,100)
